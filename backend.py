1ad88e617154f648440d6feaf3711faeb577b2ff
Paras <punaterparas@gmail.com>
2022-06-03 20:25:44 -0400

    Web interface operational for GET, POST, and PULL requests

--- backend.py
+++ backend.py
@@ -2,28 +2,40 @@ import requests
 import json
 
 
-def get_request(url, headers = {}, payload = {}):
+def get_request(url, headers = None, payload = None):
     try:
         r = requests.get(url, headers = headers, data = json.dumps(payload))
+
+        try:
+            return r.json()
+        except requests.JSONDecodeError:
+            return r.text
+
     except:
-        return 'Error'
-
-    return r.json()
+        return 'Error when attempting to make request'
 
 
-def post_request(url, headers = {}, payload = {}):
+def post_request(url, headers = None, payload = None):
     try:
         r = requests.post(url, headers = headers, data = json.dumps(payload))
+
+        try:
+            return r.json()
+        except requests.JSONDecodeError:
+            return r.text
+
     except:
-        return 'Error'
-
-    return r.json()
+        return 'Error when attempting to make request'
 
 
-def put_request(url, headers={}, payload={}):
+def put_request(url, headers = None, payload = None):
     try:
-        r = requests.put(url, headers=headers, data=json.dumps(payload))
-    except:
-        return 'Error'
+        r = requests.put(url, headers = headers, data = json.dumps(payload))
 
-    return r.json()
+        try:
+            return r.json()
+        except requests.JSONDecodeError:
+            return r.text
+
+    except:
+        return 'Error when attempting to make request'
--- main.py
+++ main.py
@@ -1,10 +1,68 @@
-from flask import Flask
+from flask import Flask, request, render_template
+import json
+from backend import get_request, post_request, put_request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods = ['GET', 'POST'])
 def index():
-    return 'Test'
+    if request.method == 'GET':
+        return render_template('index.html', ENDPOINT = '', HEADERS = '', PAYLOAD = '',
+                               RESPONSE = 'No response received yet')
+    elif request.method == 'POST':
+        endpoint = request.form.get('endpoint')
+        method = request.form.get('method')
+        headers = request.form.get('headers')
+        payload = request.form.get('payload')
 
-app.run()
+        if headers != '':
+            try:
+                # Converts headers HTML input of type str (i.e. '{"key":"value"}') to type dict (i.e. {"key":"value")
+                headers = json.loads(headers)
+
+                # JSON very particular about ' vs ", only latter is acceptable
+                # However, when Python converts dict objects to str objects, any str value within dict is given '
+                # headers_str not only converts dict to str but within string also converts any instance of ' to "
+                headers_str = str(headers).replace("'", '"')
+
+            except json.decoder.JSONDecodeError:
+                return render_template('index.html', ENDPOINT = endpoint, HEADERS = headers,
+                                       PAYLOAD = payload, RESPONSE = 'Headers incorrectly formatted')
+        else:
+            headers = None
+            headers_str = ''
+
+        if payload != '':
+            try:
+                # Converts payload HTML input of type str (i.e. '{"key":"value"}') to type dict (i.e. {"key":"value")
+                payload = json.loads(payload)
+
+                # JSON very particular about ' vs ", only latter is acceptable
+                # However, when Python converts dict objects to str objects, any str value within dict is given '
+                # payload_str not only converts dict to str but within string also converts any instance of ' to "
+                payload_str = str(payload).replace("'", '"')
+
+            except json.decoder.JSONDecodeError:
+                return render_template('index.html', ENDPOINT = endpoint, HEADERS = headers,
+                                       PAYLOAD = payload, RESPONSE = 'Payload incorrectly formatted')
+        else:
+            payload = None
+            payload_str = ''
+
+        if method == 'GET':
+            response = get_request(endpoint, headers, payload)
+            return render_template('index.html', ENDPOINT = endpoint, HEADERS = headers_str, PAYLOAD = payload_str,
+                                   RESPONSE = response)
+        elif method == 'POST':
+            response = post_request(endpoint, headers, payload)
+            return render_template('index.html', ENDPOINT = endpoint, HEADERS = headers_str, PAYLOAD = payload_str,
+                                   RESPONSE = response)
+        elif method == 'PUT':
+            response = put_request(endpoint, headers, payload)
+            return render_template('index.html', ENDPOINT = endpoint, HEADERS = headers_str, PAYLOAD = payload_str,
+                                   RESPONSE = response)
+
+
+if __name__ == '__main__':
+    app.run()
