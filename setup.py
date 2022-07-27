6c687341aa030568f72a063ec3e87c529ae22ebc
Paras <punaterparas@gmail.com>
2022-07-27 02:26:51 -0400

    Removed setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-with open('requirements.txt') as file:
-    dependencies = file.read().splitlines()
-
-setup(
-    name='Quick API Tester',
-    install_requires=dependencies
-)
